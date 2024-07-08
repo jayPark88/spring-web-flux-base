acd297c9e83fb7fb04ab5022681f2745c8607859
jayPark8282 <jaypark8282@gmail.com>
2024-07-09 00:55:18 +0900

    modify : authCheckFilter

--- src/main/java/com/jaypark8282/webFlux/api/v1/sample/controller/SampleController.java
+++ src/main/java/com/jaypark8282/webFlux/api/v1/sample/controller/SampleController.java
@@ -1,7 +1,5 @@
 package com.jaypark8282.webFlux.api.v1.sample.controller;
 
-import com.jaypark8282.webFlux.api.v1.sample.dto.UserDto;
-import com.jaypark8282.webFlux.api.v1.sample.service.SampleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +13,8 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class SampleController {
 
-    private final SampleService service;
-
-    @GetMapping("/mono/random-user")
-    public Mono<UserDto> hello() {
-        return service.getRandomUser();
+    @GetMapping("/mono")
+    public Mono<String> hello() {
+        return Mono.just("hello");
     }
 }
